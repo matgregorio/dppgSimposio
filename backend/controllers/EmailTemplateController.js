91a474c39e0afd91be2a28cad3c13a3f9563b13a
mat_gregorio15 <mateusgregorio178@gmail.com>
2024-11-07 16:09:51 -0300

    algumas funcoes adicionadas para o template controller

--- backend/controllers/EmailTemplateController.js
+++ backend/controllers/EmailTemplateController.js
@@ -1,3 +1,23 @@
 const EmailTemplate = require('../models/EmailTemplate');
 
-exports.listarTemplates = 
+module.exports = class EmailTemplateController{
+    static async listarTemplates(req,res){
+        const {nome,assunto,texto} = req.body
+    }
+
+    static async editarTemplate(req,res){
+        const id = req.params.id
+        const{nome, assunto,texto} = req.body
+        //requisição para verificar tipoUsuario
+        if(!nome){
+            res.status(422).json({message: 'O campo nome é obrigatório'})
+        }
+        if(!assunto){
+            res.status(422).json({message: 'O campo assunto é obrigatório'})
+        }
+        if(!texto){
+            res.status(422).json({message: 'O campo texto é obrigatório'})
+        }
+        
+    }
+}
